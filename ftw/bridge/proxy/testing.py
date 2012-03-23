6c768a8e09dfa6965cd95707bf3694d78215090e
Jonas Baumann <jone@jone.ch>
2012-03-23 14:26:56 +0100

    Testing setup: provide example clients.

--- ftw/bridge/proxy/testing.py
+++ ftw/bridge/proxy/testing.py
@@ -6,7 +6,18 @@ import pyramid_zcml
 class PyramidLayer(Layer):
 
     def setUp(self):
-        self.config = testing.setUp(hook_zca=True)
+        settings = {
+            'clients.foo.aliases': 'foo2',
+            'clients.foo.ip_addresses': '127.0.0.1',
+            'clients.foo.internal_url': 'http://127.0.0.1:8080/foo/',
+            'clients.foo.public_url': 'http://localhost:8080/foo/',
+
+            'clients.bar.ip_addresses': '127.0.0.1, 127.0.0.3',
+            'clients.bar.internal_url': 'http://127.0.0.1:9080/bar/',
+            'clients.bar.public_url': 'http://localhost:9080/bar/',
+            }
+
+        self.config = testing.setUp(hook_zca=True, settings=settings)
         self.config.include(pyramid_zcml)
         self.config.load_zcml('ftw.bridge.proxy:configure.zcml')
 
